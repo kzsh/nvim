d479bab6539f4d8354503e6be402c26a4d1ef073
kzsh <kzsh@kzsh.dev>
2025-11-26 18:31:46 -0500

    Add tmux shortcuts again

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -982,5 +982,29 @@ return {
   {
     'chrisbra/csv.vim',
     ft = {'csv'},
+  },
+  {
+    'yuratomo/w3m.vim',
+    lazy = false,
+    priority = 1000
+  },
+  {
+    'christoomey/vim-tmux-navigator',
+    lazy = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
   }
 }
