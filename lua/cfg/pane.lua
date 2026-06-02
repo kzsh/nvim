3ce12391542f7a20f33ea125059f23f572c1ea5e
kzsh <kzsh@kzsh.dev>
2026-06-01 22:21:08 -0400

    Convert section to lua

--- lua/cfg/pane.lua
+++ lua/cfg/pane.lua
@@ -1,9 +1,6 @@
-vim.cmd([[
-"==========================================================
-" Shortcuts for pane navigation
-"==========================================================
-nnoremap <silent><C-h> <C-w><C-h>
-nnoremap <silent><C-l> <C-w><C-l>
-nnoremap <silent><C-j> <C-w><C-j>
-nnoremap <silent><C-k> <C-w><C-k>
-]])
+local map = vim.keymap.set
+
+map("n", "<C-h>", "<C-w><C-h>", { silent = true })
+map("n", "<C-l>", "<C-w><C-l>", { silent = true })
+map("n", "<C-j>", "<C-w><C-j>", { silent = true })
+map("n", "<C-k>", "<C-w><C-k>", { silent = true })
