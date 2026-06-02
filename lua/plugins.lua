4d45debb671abbce8612fc3f6994109ef8a5323f
kzsh <kzsh@kzsh.dev>
2026-06-01 21:59:13 -0400

    Correct loading hook

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -728,7 +728,8 @@ return {
 },
 {
   'neovim/nvim-lspconfig',
-  init = function()
+  lazy = false,
+  config = function()
     -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
     -- vim.lsp.config('bashls', {})
