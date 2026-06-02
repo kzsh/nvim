dee7cff1a1e12806fbad11b0ffcdaf80fd71d34f
kzsh <kzsh@kzsh.dev>
2026-06-01 22:24:57 -0400

    Convert terminal section

--- lua/cfg/terminal.lua
+++ lua/cfg/terminal.lua
@@ -1,10 +1,27 @@
-vim.cmd([[
-"==========================================================
-" Start terminal in insert mode
-"==========================================================
-autocmd TermOpen * startinsert
-autocmd TermLeave * setlocal number
-autocmd TermEnter * setlocal nonumber
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup("Terminal", { clear = true })
 
-let g:terminal_scrollback_buffer_size = 100000
-]])
+-- Start terminal in insert mode
+autocmd("TermOpen", {
+  group = augroup,
+  pattern = "*",
+  command = "startinsert",
+})
+
+autocmd("TermLeave", {
+  group = augroup,
+  pattern = "*",
+  callback = function()
+    vim.opt_local.number = true
+  end,
+})
+
+autocmd("TermEnter", {
+  group = augroup,
+  pattern = "*",
+  callback = function()
+    vim.opt_local.number = false
+  end,
+})
+
+vim.g.terminal_scrollback_buffer_size = 100000
