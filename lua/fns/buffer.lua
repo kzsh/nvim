aeb7b6c8597d5b39de1039f8acc2b3ae5d8e8354
kzsh <kzsh@kzsh.dev>
2026-06-01 22:42:41 -0400

    Convert buffer fns

--- lua/fns/buffer.lua
+++ lua/fns/buffer.lua
@@ -1,12 +1,23 @@
-vim.cmd([[
-"==========================================================
-" Buffer functions
-"==========================================================
-function! GitRootsForAllBuffers()
-  return Filtered(function('len'), Mapped(function('FindGitRootForPath'), AllBufferFileNames()))
-endfunction
+-- Buffer functions
 
-function! AllBufferFileNames()
-  return map(filter(range(0,bufnr('$')), 'buflisted(v:val)'), "fnamemodify(bufname(v:val), ':p')")
-endfunction
-]])
+local function all_buffer_file_names()
+  local names = {}
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.fn.buflisted(buf) == 1 then
+      names[#names + 1] = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ":p")
+    end
+  end
+  return names
+end
+
+local function git_roots_for_all_buffers()
+  local utils = require("utils")
+  local roots = {}
+  for _, name in ipairs(all_buffer_file_names()) do
+    local root = utils.find_git_root_for_path(name)
+    if root ~= "" then
+      roots[#roots + 1] = root
+    end
+  end
+  return roots
+end
